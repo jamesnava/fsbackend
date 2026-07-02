ce5ba4088355e91ece4fcef474e5b850cbe1d2bd
Jaime <navarrocruzjaime@gmail.com>
2026-07-02 16:25:59 -0500

    02072026

--- procesamiento.py
+++ procesamiento.py
@@ -1,19 +1,29 @@
 
-import pytesseract
+
 import cv2
 import numpy as np
 import httpx
 import re
+from fastapi import HTTPException
 
 
 async def leerImage(ruta_archivo:str):
 	datos={"apikey":"K85162706288957","language":'eng'}
-	with open(ruta_archivo,"rb") as f:
-		archivos={"file":(ruta_archivo,f,'image/png')}
 
-		async with httpx.AsyncClient() as cliente:
-			respuesta=await cliente.post("https://api.ocr.space/parse/image",data=datos,files=archivos)
-			resultado_json=respuesta.json()
+	try:
+		with open(ruta_archivo,"rb") as f:
+			archivos={"file":(ruta_archivo,f,'image/png')}
+			async with httpx.AsyncClient(timeout=60.0) as cliente:
+				respuesta=await cliente.post("https://api.ocr.space/parse/image",data=datos,files=archivos)
+				resultado_json=respuesta.json()
+	except httpx.ReadTimeout:
+		raise HTTPException(status_code=504,detail="ORC API no respondio a tiempo")
+	except Exception as e:
+		raise HTTPException(status_code=500,detail=f"error inesperado: {str(e)}")
+
+	if "ParsedResults" not in resultado_json:
+		raise HTTPException(status_code=400,detail=resultado_json.get("ErrorMessage","Error en OCR"))
+		
 	texto_extraido=resultado_json["ParsedResults"][0]["ParsedText"]
 	final=ObtencionCodigo(texto_extraido)
 	print(final)
@@ -37,16 +47,18 @@ async def preprocesamiento(img,roi):
 	return resultado
 
 def processImage(img):
-	binary_adapt=cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,21,2)
-	kernel=np.ones((3,3),np.uint8)
-	opening=cv2.morphologyEx(binary_adapt,cv2.MORPH_OPEN,kernel)
-	return opening
+	gray_blur = cv2.bilateralFilter(img, 9, 75, 75)
+	#gray_blur=cv2.medianBlur(img,3)
+	_,binary_adapt=cv2.threshold(gray_blur,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+	#kernel=np.ones((3,3),np.uint8)
+	#opening=cv2.morphologyEx(binary_adapt,cv2.MORPH_OPEN,kernel)
+	return binary_adapt
 
 def ObtencionCodigo(texto):
-	patronMStatus=r"M-Status:\s*(w+)"
-	patronSData=r"S-Data (Hex):\n\s*(\w+)"
-	patronMData=r"M-Data (Hex):\n\s*([\w ]+)"
-	patronTCode=r"T-Code:\s*(d+)"
+	patronMStatus=r"M-Status:\s*(\w+)"
+	patronSData=r"S-Data\s*\(Hex\):\s*([\w]+)"
+	patronMData=r"M-Data\s*\(Hex\):\s*([\w ]+)"
+	patronTCode=r"T-Code:\s*(\d+)"
 	patrones={"m_status":patronMStatus,"s_data":patronSData,
 	"s_data":patronMData,"t_code":patronTCode}
 	resultados={}
